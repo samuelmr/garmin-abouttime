d50dece1fc31bc5a8b4747b5e50ebb3a8d84811f
Samuel Rinnetmäki <samuel@iki.fi>
2019-03-09 12:45:34 +0200

    script to extract required characters from translated strings

---
+++ filter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TMPFILE1=./filter.tmp
+TMPFILE2=./filter2.tmp
+touch $TMPFILE1
+for filename in ./resources-*/strings.xml; do
+  [ -e "$filename" ] || continue
+  # echo $filename
+  # cat $filename | tr '\n' ' ' | sed -e 's/[ \|]*//g' -e 's/<[^>]*>//g'
+  cat $filename | tr '\n' ' ' | sed -e 's/ //g' -e 's/<[^>]*>//g' >> $TMPFILE1
+done
+grep -o . $TMPFILE1 | sort -u > $TMPFILE2
+cat $TMPFILE2 | tr '\n' ' ' | sed -e 's/ //g' | sed -e 's/|/ /g'
+rm $TMPFILE1 $TMPFILE2
