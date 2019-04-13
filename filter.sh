cc37aa49addfa0920652d03e9c01bb72546b8892
Samuel Rinnetmäki <samuel@iki.fi>
2019-04-13 10:45:23 +0300

    strip $ and $ from filter

--- filter.sh
+++ filter.sh
@@ -6,7 +6,7 @@ for filename in ./resources-*/strings.xml; do
   [ -e "$filename" ] || continue
   # echo $filename
   # cat $filename | tr '\n' ' ' | sed -e 's/[ \|]*//g' -e 's/<[^>]*>//g'
-  cat $filename | tr '\n' ' ' | sed -e 's/ //g' -e 's/<[^>]*>//g' >> $TMPFILE1
+  cat $filename | tr '\n' ' ' | sed -e 's/ //g' -e 's/\$.\$//g' -e 's/<[^>]*>//g' >> $TMPFILE1
 done
 grep -o . $TMPFILE1 | sort -u > $TMPFILE2
 cat $TMPFILE2 | tr '\n' ' ' | sed -e 's/ //g' | sed -e 's/|/ /g'
