2245367baccefe72764c9f221d7f954e2d66103a
Samuel Rinnetmäki <samuel@iki.fi>
2019-10-27 15:52:59 +0200

    update resources.xml in language specific resource folders

--- filter.sh
+++ filter.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 TMPFILE1=./filter.tmp
 TMPFILE2=./filter2.tmp
-echo "0123456789% " > $TMPFILE1
-for filename in ./resources-*/strings.xml; do
-  [ -e "$filename" ] || continue
-  # echo $filename
-  # cat $filename | tr '\n' ' ' | sed -e 's/[ \|]*//g' -e 's/<[^>]*>//g'
-  cat $filename | tr '\n' ' ' | sed -e 's/ //g' -e 's/\$.\$//g' -e 's/<[^>]*>//g' >> $TMPFILE1
+for directory in ./resources-*; do
+	if [ $directory = "./resources-smallscreen" ]
+	then
+	  continue
+	fi
+	cd $directory
+  [ -L "bitmaps.xml" ] || ln -s ../resources/bitmaps.xml .
+  [ -L "images" ] || ln -s ../resources/images .
+  [ -e "resource" ] || mkdir resource
+	cd resource
+  [ -L "fonts" ] || ln -s ../../resources/resource/fonts .
+  [ -e "resources.xml" ] || cp ../../resources/resource/resources.xml .
+  echo "0123456789% " > $TMPFILE1
+	cat ../strings.xml | tr '\n' ' ' | sed -e 's/ //g' -e 's/\$.\$//g' -e 's/<[^>]*>//g' >> $TMPFILE1
+  grep -o . $TMPFILE1 | sort -u > $TMPFILE2
+  cat $TMPFILE2 | tr '\n' ' ' | sed -e 's/ //g' | sed -e 's/	/ /g' | sed -e 's/|/ /g' > $TMPFILE1
+  FILTERSTR=`cat $TMPFILE1`
+	echo "${directory}	${FILTERSTR}"
+	sed -e "s/filter=\".*\"/filter=\"${FILTERSTR}\"/g" resources.xml > $TMPFILE2
+	mv $TMPFILE2 resources.xml
+  rm $TMPFILE1
+	cd ../..
 done
-grep -o . $TMPFILE1 | sort -u > $TMPFILE2
-cat $TMPFILE2 | tr '\n' ' ' | sed -e 's/ //g' | sed -e 's/|/ /g'
-rm $TMPFILE1 $TMPFILE2
