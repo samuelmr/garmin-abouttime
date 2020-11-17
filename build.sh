b0dc22c396229d07fa94c79c19a9edaf7e73334b
Samuel Rinnetmäki <samuel@iki.fi>
2020-11-17 14:57:48 +0200

    possible to build a single translation

--- build.sh
+++ build.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+TRANSLATION=""
 TEST=""
 TESTMESSAGE=" for release"
-if [ "$1" = "-t" ]
-then
+if [ "$1" = "-t" ]; then
   TEST="--unit-test"
   TESTMESSAGE=" with unit tests for testing"
+elif [ -z "$1"]; then # empty; compile all translations
+  TRANSLATION="*"
+else
+  TRANSLATION=$1
 fi
 
 : ${CIQ_TARGET:="fenix5plus"}
 PROJECT=`basename $PWD`
 TARGETPOSTFIX=""
-if [ $CIQ_TARGET = "fr920xt" ]
-then
+if [ $CIQ_TARGET = "fr920xt" ]; then
   TARGETPOSTFIX="_sim"
 fi
 
@@ -21,11 +24,10 @@ DEVELOPER_KEY="/Users/samuel/garmin/developer_key.der"
 echo "Updating font filters"
 ./filter.sh
 
-for jungle in ./monkey-*.jungle; do
+for jungle in ./monkey-$TRANSLATION.jungle; do
   LANGUAGE=`echo $jungle | sed -e "s/^.*monkey-\(.*\).jungle.*$/\1/g"`
   echo "Building $LANGUAGE$TESTMESSAGE"
-  if [ $TEST ]
-  then
+  if [ $TEST ]; then
     filename="releases/AboutTime-$LANGUAGE-$CIQ_TARGET.prg"
     CMD="monkeyc -f $jungle -d ${CIQ_TARGET}${TARGETPOSTFIX} -y $DEVELOPER_KEY -o $filename $TEST"
   else
