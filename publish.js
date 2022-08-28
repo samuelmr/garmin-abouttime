d1c0b2342d163edacd41913bd7ca2cb37a3ef778
Samuel Rinnetmäki <samuel@iki.fi>
2022-08-28 21:31:48 +0300

    Added automated publishing script

---
+++ publish.js
@@ -0,0 +1,65 @@
+const puppeteer = require('puppeteer')
+const config = require('./config.js')
+const translations = require('./translations.json')
+
+const PAGE_TIMEOUT_S = 120
+
+const loginPage = 'https://apps.garmin.com/login';
+
+(async () => {
+  const browser = await puppeteer.launch({headless: true})
+  const page = await browser.newPage()
+  page.setDefaultNavigationTimeout(PAGE_TIMEOUT_S * 1000)
+  await page.goto(loginPage, {waitUntil: 'networkidle2'})
+
+  const elementHandle = await page.$('#gauth-widget-frame-gauth-widget')
+  if (elementHandle) { // else assume we're authenticated
+    const frame = await elementHandle.contentFrame()
+    await frame.type('#username', config.username)
+    await frame.type('#password', config.password)
+    await frame.click('#login-btn-signin')
+    await page.waitForNavigation()
+  }
+
+  let counter = 0;
+  for (uuid in translations) {
+    try {
+      const url = `https://apps.garmin.com/en-US/developer/${config.dev}/apps/${uuid}/update`
+      await page.goto(url, {waitUntil: 'networkidle2'})
+
+      const consentHandle = await page.$('#truste-consent-button')
+      if (consentHandle) {
+        await page.click('#truste-consent-button')
+        await new Promise(r => setTimeout(r, 2 * 1000))
+      }
+
+      const input = await page.$('input[name=file]')
+      await input.uploadFile(`./releases/AboutTime-${translations[uuid]}.iq`)
+      await new Promise(r => setTimeout(r, 1 * 1000))
+      await page.type('#app-version', config.version)
+      await page.click('button[name="submit"]')
+      await page.waitForNavigation();
+
+      const successtHandle = await page.$('.text-success')
+      if (successtHandle) {
+        console.log(`Published v. ${config.version} of ${translations[uuid]}`)
+        ++counter
+      }
+      else {
+        console.warn(`Error publishing v. ${config.version} of ${translations[uuid]}`)
+        const alertHandle = await page.$('.alert-danger')
+        if (alertHandle) {
+          let value = await page.evaluate(el => el.textContent, alertHandle)
+          console.log(value)
+        }
+      }
+    }
+    catch(e) {
+      console.error(`Error publishing v. ${config.version} of ${translations[uuid]}`)
+      throw new Error(e)
+    }
+  }
+  console.log(`Published ${counter} translation${counter != 1 ? 's' : ''}.`)
+  await browser.close();
+})()
+
